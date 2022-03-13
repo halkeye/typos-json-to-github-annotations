21da1ae33d78baf8853ae943e7611f5946706ea1
Gavin Mogan <git@gavinmogan.com>
2022-03-13 11:52:11 -0700

    Initial working version

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use clap::Parser;
+use serde::Deserialize;
+use std::fmt;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+#[derive(Parser)]
+struct Cli {
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+#[derive(Deserialize)]
+pub struct Typo {
+    path: String,
+    line_num: i32,
+    byte_offset: i32,
+    typo: String,
+    corrections: Vec<String>,
+}
+
+impl fmt::Display for Typo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "::error file={},line={},col={}::{} should be {}",
+            self.path,
+            self.line_num,
+            self.byte_offset,
+            self.typo,
+            itertools::join(self.corrections.iter().map(|s| format!("`{}`", s)), ", "),
+        )
+    }
+}
+
+#[derive(Deserialize)]
+pub struct BinaryFile {
+    // path: String
+}
+
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+enum Message {
+    #[serde(rename = "binary_file")]
+    BinaryFile(BinaryFile),
+    #[serde(rename = "typo")]
+    Typo(Typo)
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::parse();
+
+    let f = File::open(&args.path)?;
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        let unwrapped_line = line.unwrap();
+        let msg: Message = serde_json::from_str(&unwrapped_line)?;
+        match msg {
+            Message::BinaryFile(_) => {}
+            Message::Typo(typo) => println!("{}", typo),
+        }
+    }
+
+    Ok(())
+}
+
+/*
+*/
